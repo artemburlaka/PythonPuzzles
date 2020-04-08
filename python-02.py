61824b2994264f9eb7ad2b17ab4d0b1919fc4d8e
aburlaka <aburlaka@cisco.com>
2020-04-08 12:39:06 +0300

    the final result

--- python-02.py
+++ python-02.py
@@ -14,7 +14,7 @@ def path_to_file():
 
 
 def one_more():
-    answer_one_more = input("Would you like to analyze one more file? (y/n)\n\t")
+    answer_one_more = input("\nWould you like to analyze one more file? (y/n)\n\t")
     if answer_one_more == "y":
         return True
     elif answer_one_more == "n":
@@ -26,20 +26,32 @@ def one_more():
 
 def statistic_from_file(file):
     opened_file = open(file, "r")
-    dict_for_statistic = {"num_lines": 0, "empty_lines": 0}
+    dict_for_statistic = {
+        "total lines": 0,
+        "empty lines": 0,
+        "lines with 'z'": 0,
+        "'z' count": 0,
+        "lines with 'and'": 0
+    }
     for line in opened_file:
-        dict_for_statistic["num_lines"] += 1
+        dict_for_statistic["total lines"] += 1
         if line in ['\n', '\r\n']:
-            dict_for_statistic["empty_lines"] += 1
-
+            dict_for_statistic["empty lines"] += 1
+        if "z" in line:
+            dict_for_statistic["lines with 'z'"] += 1
+            dict_for_statistic["'z' count"] += line.count("z")
+        if line.startswith("and ") or line.startswith("and\n") :
+            dict_for_statistic["lines with 'and'"] += 1
+        if line.endswith(" and\n"):
+            dict_for_statistic["lines with 'and'"] += 1
+        if " and " in line:
+            dict_for_statistic["lines with 'and'"] += line.count(" and ")
     print_statistic(**dict_for_statistic)
 
 
 def print_statistic(**kwargs):
     for key, value in kwargs.items():
-        print("{0} = {1}".format(key, value))
-    # print(dict_for_statistic["num_lines"])
-    # print(dict_for_statistic["empty_lines"])
+        print("{0}: {1}".format(key, value))
 
 
 if __name__ == '__main__':
